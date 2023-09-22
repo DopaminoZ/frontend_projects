01dc2f8e4727b02e686fa2fb1302cab63499cbde
DopaminoZ <ali.bagoury@gmail.com>
2023-09-22 04:21:24 +0300

    fixed a few stuff in timer

--- timer/js/app.js
+++ timer/js/app.js
@@ -1,6 +1,5 @@
 const start=document.querySelector('#start');
 const stop=document.querySelector('#stop');
-let audio=document.querySelector('#beepz');
 let hrs=0
 let mins=0
 let secs=0
@@ -11,6 +10,14 @@ mins=document.querySelector('#mins');
 secs=document.querySelector('#secs');
 
 function timer(){
+  if(secs.value>60){
+    mins.value=parseInt(secs.value/60)
+    secs.value=secs.value%60
+  }
+  if(mins.value>60){
+    hrs.value=parseInt(mins.value/60)
+    mins.value=mins.value%60
+  }
   if (hrs.value>0 && secs.value==0 && mins.value==0){
     hrs.value--;
     mins.value=59;
@@ -25,12 +32,20 @@ function timer(){
   }
   if(secs.value==0 && mins.value==0 && hrs.value==0){
     alert("Timer is over!")
-    status="off"
-    window.clearInterval(timerInt)
-    start.innerHTML='<i class="fa-solid fa-play"></i>';
-    document.getElementById('#beepz').play()
+    reset()
   }
 }
+
+function reset(){
+  window.clearInterval(timerInt)
+  secs.value=null
+  mins.value=null
+  hrs.value=null
+  status="off"
+  start.innerHTML='<i class="fa-solid fa-play"></i>';}
+
+stop.addEventListener('click', reset);
+
 
 start.addEventListener('click', function(){
   if(status=="off"){
@@ -43,15 +58,8 @@ start.addEventListener('click', function(){
     status="off"
   }
   });
-stop.addEventListener('click', function(){
-    if(1){
-      window.clearInterval(timerInt)
-      secs.value=0
-      mins.value=0
-      hrs.value=0
-      status="off"
-      start.innerHTML='<i class="fa-solid fa-play"></i>';}
-    });
+
+
   
 //   var x = document.getElementById("#beepz"); 
 
